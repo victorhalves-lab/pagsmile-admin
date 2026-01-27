7c16a65c58b5554ea3585ae7a57d4a61c0e725f4
Victor Hugo Alves <victorhalves@gmail.com>
2026-01-27 21:55:06 +0000

    File changes

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -304,7 +304,7 @@ export default function Customers() {
             </Button>
           </div>
         </div>
-      )
+      )}
 
       {/* KPI Cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
