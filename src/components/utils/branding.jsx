c76dd6dbccce0bb463b017e5a9a2afac9a251c12
Victor Hugo Alves <victorhalves@gmail.com>
2026-01-29 04:27:07 +0000

    File changes

--- src/components/utils/branding.jsx
+++ src/components/utils/branding.jsx
@@ -19,5 +19,5 @@ export const getLogoUrlByTheme = (theme) => {
   if (theme === 'light') {
     return "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6979104cafd6b02cfed66766/2cf8bf7b4_Logo-modo-claro.png";
   }
-  return "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6979104cafd6b02cfed66766/6bc1f8b3d_Logo-modo-escuro.png";
+  return "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6979104cafd6b02cfed66766/7fc82cec8_Logo-modo-escuro.png";
 };
